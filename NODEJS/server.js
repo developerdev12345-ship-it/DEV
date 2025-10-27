85c4e19b5e32356b6f7139c71516b88ecabdc783
developerdev12345-ship-it <developer.dev12345@gmail.com>
2025-10-27 07:12:45 +0530

    Update server.js

--- NODEJS/server.js
+++ NODEJS/server.js
@@ -17,7 +17,8 @@ io.on("connection",(socket)=>{
     })
 });
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => {
   console.log(`Server running`);
+
 });
